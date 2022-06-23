4e5b6c4e0d9dd8fac74a406ad8a2975408998957
brando <belicarrillo30@gmail.com>
2022-06-23 11:39:30 -0500

    agrupación directorios en repositorio y nuevo ejercicio clases estáticas realizado

---
+++ edad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App2_curso_Udemy_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persona1 = new Persona() { fechaNacimiento = new DateTime(1980, 1, 5) };
+            Console.WriteLine($"La edad de la persona es {persona1.Edad}");
+        }
+    }
+
+    public class Persona
+    {
+        public Persona()
+        {
+            Console.WriteLine("constructor Persona");
+            Telefonos = new List<string>();
+        }
+
+        private DateTime _fechaNacimiento;
+        public int Edad { get { return UtilidadesDeFechas.CalcularEdad(fechaNacimiento); } }
+        public List<string> Telefonos { get; set; }
+        public DateTime fechaNacimiento {
+            get { return _fechaNacimiento; }
+            set { _fechaNacimiento = value; }
+        }
+    }
+}
---
+++ edad/UtilidadesDeFechas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_curso_Udemy_Csharp
+{
+    public static class UtilidadesDeFechas
+    {
+        public static int CalcularEdad(DateTime fechaNacimiento)        // Método estático
+        {
+            var edad = DateTime.Today.Year - fechaNacimiento.Year;
+            var temp = new DateTime(DateTime.Today.Year,
+                fechaNacimiento.Month, fechaNacimiento.Day);
+            
+            if (temp > DateTime.Today)
+            {
+                edad--;
+            }
+            return edad;
+        }
+    }
+}
